c4f02e6190b97e8327000907262669095b02543d
Masoud <mmtaee64@gmail.com>
2024-05-01 16:15:07 +0330

    add privatekey loader

--- modian.go
+++ modian.go
@@ -2,6 +2,7 @@ package moadian
 
 import (
 	"crypto/rsa"
+	"github.com/mmtaee/moadian/internal/encryption"
 	"github.com/mmtaee/moadian/internal/tax_id"
 	"time"
 )
@@ -17,6 +18,10 @@ type Service struct {
 	TaxUrl             string
 	Version            string
 	Priority           string
+}
+
+func PrivateKeyFromPath(path string) (*rsa.PrivateKey, error) {
+	return encryption.LoadPrivateKey(path)
 }
 
 func New(privateKey *rsa.PrivateKey, fiscalID string, economicCode string) *Service {
